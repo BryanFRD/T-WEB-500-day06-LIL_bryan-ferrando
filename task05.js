a318679f2077e3ac115c0a61ab3a1de52b91897f
Bryan <bryan.ferrando@epitech.eu>
2023-10-02 08:56:44 +0100

    task05

---
+++ task05.js
@@ -0,0 +1,14 @@
+export function range(start, end, step = 1){
+  const t = [];
+  if(start > end){
+    for(let i = start; i >= end; i += step){
+      t.push(i);
+    }
+    return t;
+  }
+  
+  for(let i = start; i <= end; i += step){
+    t.push(i);
+  }
+  return t;
+}
